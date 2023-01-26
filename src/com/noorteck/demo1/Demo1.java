1719a44136a969cda752b0ed7250dab0468be6f7
Tima <Tima@LAPTOP-2U2N3SO0.mynetworksettings.com>
2023-01-25 22:04:32 -0500

    initial commit

---
+++ src/com/noorteck/demo1/Demo1.java
@@ -0,0 +1,11 @@
+package com.noorteck.demo1;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is Java Class available in my Local workspace");
+		
+	}
+
+}
